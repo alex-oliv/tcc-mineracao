8685789c13548a64857100ce2a4dadee238ce770
Alexander Victor <alexander.oliveira@a7participacoes.com>
2022-06-08 20:24:57 -0300

    all issues dates almost ready

--- main.py
+++ main.py
@@ -63,69 +63,81 @@ def parse_dates(count, dates, issue_type):
     return count
 
 
-issues = 0
-pulls = 0
-parser = argparse.ArgumentParser(
-    description="Simple parser for GitHub issues and pull requests"
-)
-parser.add_argument("-t", "--token", '--nargs', nargs='+', help="GitHub token")
-parser.add_argument("-r", "--repo", help="GitHub repository, as 'owner/repo'")
-args = parser.parse_args()
+def built_json():
+    issues = 0
+    pulls = 0
+    parser = argparse.ArgumentParser(
+        description="Simple parser for GitHub issues and pull requests"
+    )
+    parser.add_argument("-t", "--token", '--nargs',
+                        nargs='+', help="GitHub token")
+    parser.add_argument(
+        "-r", "--repo", help="GitHub repository, as 'owner/repo'")
+    args = parser.parse_args()
 
-(owner, repo) = args.repo.split('/')
-# commits = get_commits(owner, repo)
-repo = GitHub(owner=owner, repository=repo, api_token=args.token)
+    (owner, repo) = args.repo.split('/')
+    # commits = get_commits(owner, repo)
+    repo = GitHub(owner=owner, repository=repo, api_token=args.token)
 
+    aux = []
+    count = 0
+
+    for item in repo.fetch():
+        if 'pull_request' in item['data']:
+            pulls += 1
+        else:
+            labels = parse_arrays(item['data']['labels'], 'name')
+            assignees = len(parse_arrays(
+                item['data']['assignees'], 'login'))
+
+            aux.append({'number': item['data']['number'],
+                        'creator': item['data']['user']['login'],
+                        'state': item['data']['state'],
+                        'labels': labels,
+                        'assignees': assignees,
+                        'comments': item['data']['comments'],
+                        'created_at': item['data']['created_at'],
+                        'closed_at': item['data']['closed_at']
+                        })
+            issues += 1
+
+        count += 1
+        if count == 5:
+            break
+        # lines.append(f"{item}\n")
+
+    result = {}
+    result['issues'] = aux
+    lines.append(json.dumps(result))
+    dadad = 1
+    # print(
+    #     f"Repository > Commits: {dadad} || Issues: {issues} || Pull Requests: {pulls}")
+
+    with open('result.json', 'w+') as writer:
+        writer.writelines(lines)
+        writer.close()
+
+    # created_dates = parse_dates(dates_created)
+    # closed_dates = parse_dates(dates_closed)
+    return json.dumps(result)
+    # print(created_dates)
+    # print(closed_dates)
+
+
+data = json.loads(built_json())
 dates_created = []
 dates_closed = []
-aux = []
-count = 0
-
-for item in repo.fetch():
-    if 'pull_request' in item['data']:
-        pulls += 1
-    else:
-        info['created_at'] = item['data']['created_at']
-        date = info['created_at'].split('T')[0].replace('-', '')
-        dates_created.append(date)
-        info['closed_at'] = item['data']['closed_at']
-        date = info['closed_at'].split('T')[0].replace('-', '')
-        dates_closed.append(date)
-
-        labels = parse_arrays(item['data']['labels'], 'name')
-        assignees = len(parse_arrays(
-            item['data']['assignees'], 'login'))
-
-        aux.append({'number': item['data']['number'],
-                    'creator': item['data']['user']['login'],
-                    'state': item['data']['state'],
-                    'labels': labels,
-                    'assignees': assignees,
-                    'comments': item['data']['comments'],
-                    'created_at': item['data']['created_at'],
-                    'closed_at': item['data']['closed_at']
-                    })
-        issues += 1
-
-    count += 1
-    if count == 10:
-        break
-    lines.append(f"{item}\n")
-
-result = {}
-result['issues'] = aux
-# lines.append(json.dumps(result))
-dadad = 1
-print(
-    f"Repository > Commits: {dadad} || Issues: {issues} || Pull Requests: {pulls}")
-
-with open('result-github.json', 'w+') as writer:
-    writer.writelines(lines)
-    writer.close()
+count = {}
 
 
-created_dates = parse_dates(dates_created)
-closed_dates = parse_dates(dates_closed)
+for issue in data['issues']:
+    dates_created.append(issue['created_at'].split('T')[0].replace('-', ''))
+    dates_closed.append(issue['closed_at'].split('T')[0].replace('-', ''))
 
-# print(created_dates)
-# print(closed_dates)
+print(dates_created)
+print(dates_closed)
+
+count = parse_dates(count, dates_created, "created")
+print(count)
+count = parse_dates(count, dates_closed, "closed")
+print(count)
